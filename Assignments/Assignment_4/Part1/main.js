7331b9596dc24c63cd0b49bd0072f5b2100acef7
BidhyashreeDahal <bidhyashree.dahal@dcmail.ca>
2024-08-03 11:15:30 -0400

    Update
    Completing the result function. Declared the newStory variable and set it equal to the story text. Declared three new variable xItem, yItem and zIteam and made then equal to the random value from array.

--- Assignments/Assignment_4/Part1/main.js
+++ Assignments/Assignment_4/Part1/main.js
@@ -15,3 +15,29 @@ let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. Whe
 const insertX =["Willy the Goblin", "Big Daddy", "Father Christmas"]
 const insertY =["the soup kitchen","Disneyland","the White House"]
 const insertZ=["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
+//EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent =;
+  story.style.visibility = 'visible';
+}
+
