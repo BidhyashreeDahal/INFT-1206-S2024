dfbfcec8a1dfcc1233d4ea66f1fb080cf8e30b93
BidhyashreeDahal <bidhyashree.dahal@dcmail.ca>
2024-08-03 11:58:33 -0400

    Update
    replaced the bob with the  custom name .Converted pounds into stone and fahrenheit into centigrade. Replaced the 94 fahrenheit to the value  temperature  in centigrade and 300 pounds to the value of weight in stone so that when the user hit the different radio button it shows different value.
    Lastly set story.textContent = newStory

--- Assignments/Assignment_4/Part1/main.js
+++ Assignments/Assignment_4/Part1/main.js
@@ -32,16 +32,18 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory =newStory.replaceAll('Bob',name)
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature = `${Math.round((94-32)*5/9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit',temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
 
   }
 
-  story.textContent =;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
 
