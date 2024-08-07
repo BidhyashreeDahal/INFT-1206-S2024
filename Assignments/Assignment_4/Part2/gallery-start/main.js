c987ba3195af2b45e2d7bac7aac7b96054ab08ba
BidhyashreeDahal <bidhyashree.dahal@dcmail.ca>
2024-08-06 21:57:01 -0400

    Update
    Declare the array of image file name. Declare the alt text for each file name

--- Assignments/Assignment_4/Part2/gallery-start/main.js
+++ Assignments/Assignment_4/Part2/gallery-start/main.js
@@ -5,9 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [pic1.jpg,pic2.jpg,pic3.jpg,pic4.jpg,pic5.jpg];
 
 /* Declaring the alternative text for each image file */
-
+const imageAlts={
+    pic1:"A human eye",
+    pic2:"A wavy rock",
+    pic3:"Purple flowers",
+    pic4:"A wall painted with the egyptian history",
+    pic5:"A butter over a big leaf"
+}
 /* Looping through images */
 
 const newImage = document.createElement('img');
